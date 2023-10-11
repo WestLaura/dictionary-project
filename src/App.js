06a812c5db7bf6055f3246b53c9805cc4da4c749
WestLaura <132126791+WestLaura@users.noreply.github.com>
2023-10-11 06:00:02 +0100

    removed img

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Dictionary from "./Dictionary";
+import "./Dictionary";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
+        <header className="App-header"></header>
         <main>
           <Dictionary />
         </main>
