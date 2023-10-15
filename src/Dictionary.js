2dbc70c451a3733043cb4375ee42a687752e7b8d
WestLaura <132126791+WestLaura@users.noreply.github.com>
2023-10-15 16:57:09 +0100

    integrated API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,22 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
+
+//apikey: 9a169bbfe3916405e3fo154f72be9t4a
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let apiKey = "9a169bbfe3916405e3fo154f72be9t4a";
+
+  function handleResponse(response) {
+    console.log(response);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
